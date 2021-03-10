d9a3472d1374700e86515a81733728597af08675
thomasberrens <48439074+thomasberrens@users.noreply.github.com>
2021-03-10 13:58:33 +0100

    first commit

---
+++ Assets/Scripts/Enemy/EnemyGFX.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+
+public class EnemyGFX : MonoBehaviour
+{
+    public AIPath aiPath;
+
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);            
+        } else if (aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);     
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    private float horizontalmove = 0f;
+
+    public float runSpeed = 40f;
+
+    private bool jump;
+    private bool crouch;
+
+    // Update is called once per frame
+    void Update()
+    {
+      horizontalmove = Input.GetAxisRaw("Horizontal") * runSpeed;
+      animator.SetFloat("Speed", Math.Abs(horizontalmove));
+      
+      if (Input.GetButtonDown("Jump"))
+      {
+          jump = true;
+          animator.SetBool("isJumping", true);
+      }
+
+      if (Input.GetButtonDown("Crouch"))
+      {
+          Debug.Log("Setting crouch true");
+          crouch = true;
+      } else if (Input.GetButtonUp("Crouch"))
+      {
+          Debug.Log("Setting crouch false");
+          crouch = false;
+      }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalmove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+    public void onLand()
+    {
+        animator.SetBool("isJumping", false);
+    }
+
+    public void onCrouching(bool isCrouching)
+    {
+        animator.SetBool("isCrouching", isCrouching);
+    }
+}
